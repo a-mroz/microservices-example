5ef8962c0414b019b08b3d8a6f05e392f8a290bf
Adam Mróz <adam.michal.mroz@gmail.com>
2021-01-22 20:49:15 +0100

    New submodule - shop

---
+++ shop/src/main/java/dev/amroz/microservices/bookshop/shop/ShopApplication.java
@@ -0,0 +1,10 @@
+package dev.amroz.microservices.bookshop.shop;
+
+import io.micronaut.runtime.Micronaut;
+
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(ShopApplication.class, args);
+    }
+}
