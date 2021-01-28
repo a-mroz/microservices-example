58d3954c9168ea3073fe5dd98efbc592b3e9afcb
Adam Mróz <adam.michal.mroz@gmail.com>
2021-01-28 20:25:05 +0100

    Add retrying to fetch catalog data

--- shop/src/main/java/dev/amroz/microservices/bookshop/shop/infrastructure/CatalogClient.java
+++ shop/src/main/java/dev/amroz/microservices/bookshop/shop/infrastructure/CatalogClient.java
@@ -6,11 +6,16 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Retryable;
 import io.reactivex.Flowable;
 
 @Client("http://localhost:8081")
 public interface CatalogClient extends CatalogService {
 
+    @Retryable(
+        attempts = "${catalog.books.retry.attempts:3}",
+        multiplier = "${catalog.books.retry.multiplier:1}",
+        delay = "${catalog.books.retry.delay:1s}")
     @Override
     @Get("/books")
     @Consumes(MediaType.APPLICATION_JSON_STREAM)
