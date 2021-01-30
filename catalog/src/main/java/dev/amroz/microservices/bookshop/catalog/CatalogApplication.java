935e64a45a9d11ae9715fbfa753a315c065a7450
Adam Mróz <adam.michal.mroz@gmail.com>
2021-01-30 20:20:49 +0100

    Add openapi support

--- catalog/src/main/java/dev/amroz/microservices/bookshop/catalog/CatalogApplication.java
+++ catalog/src/main/java/dev/amroz/microservices/bookshop/catalog/CatalogApplication.java
@@ -1,7 +1,16 @@
 package dev.amroz.microservices.bookshop.catalog;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Catalog Application",
+        version = "0.0.1",
+        description = "Microservice responsible for serving catalog data."
+    )
+)
 public class CatalogApplication {
 
     public static void main(String[] args) {
--- shop/src/main/java/dev/amroz/microservices/bookshop/shop/ShopApplication.java
+++ shop/src/main/java/dev/amroz/microservices/bookshop/shop/ShopApplication.java
@@ -1,7 +1,18 @@
 package dev.amroz.microservices.bookshop.shop;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Shop Application",
+        version = "0.0.1",
+        description = "Toy application for playing with Microservices"
+    )
+)
 public class ShopApplication {
 
     public static void main(String[] args) {
