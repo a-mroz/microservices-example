8a89d0e3ec4bd9738f48343ebfbea8b30f0eb791
Adam Mróz <adam.michal.mroz@gmail.com>
2021-02-18 06:35:40 +0100

    Fix package for RecommendationsTest

--- recommendations/src/test/java/dev/amroz/microservices/bookshop/recommendations/RecommendationsTest.java
+++ recommendations/src/test/java/dev/amroz/microservices/bookshop/recommendations/RecommendationsTest.java
@@ -1,4 +1,4 @@
-package dev.amroz.microservices.bookshop;
+package dev.amroz.microservices.bookshop.recommendations;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
