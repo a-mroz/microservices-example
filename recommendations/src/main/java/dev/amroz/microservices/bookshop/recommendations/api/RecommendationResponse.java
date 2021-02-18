56cf8034adf54af29c17851a426f3c45d7ddb392
Adam Mróz <adam.michal.mroz@gmail.com>
2021-02-18 07:01:01 +0100

    An attempt to generate openapi schema for RecommendationResponse

--- recommendations/src/main/java/dev/amroz/microservices/bookshop/recommendations/api/RecommendationResponse.java
+++ recommendations/src/main/java/dev/amroz/microservices/bookshop/recommendations/api/RecommendationResponse.java
@@ -1,11 +1,21 @@
 package dev.amroz.microservices.bookshop.recommendations.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Schema(description = "Recommendations")
 class RecommendationResponse {
 
+    @Schema(required = true, description = "Book title", example = "Moby Dick")
     @JsonProperty
     private final String title;
+
+    // Schema is not generated if this is non-public or when there is no getter
+    /*public*/ String getTitle() {
+        return title;
+    }
+
+
 }
